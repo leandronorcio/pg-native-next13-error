2fbf981119f5050f4f8ca46c13077fb82ecdff12
Leandro Norcio <onsongnorcio@gmail.com>
2023-04-11 10:07:01 +0800

    Made changes to reproduce the bug.

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,3 +1,19 @@
+import { Client } from 'pg';
+
 export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+  const client = new Client({
+    host: 'localhost',
+    port: 5432,
+    database: 'notes_app_nextjs13',
+    user: 'postgres',
+    password: 'password',
+  });
+  client.connect((err) => {
+    if (err) {
+      console.error('connection error', err.stack);
+    } else {
+      console.log('connected');
+    }
+  });
+  return new Response('Hello, Next.js!');
 }
